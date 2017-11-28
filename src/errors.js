3c79b4252ad8f0372a501d014616270131fddc32
Andre Ogle <ogleandre@gmail.com>
2017-11-28 15:35:11 +0200

    Initial commit

---
+++ src/errors.js
@@ -0,0 +1,6 @@
+function errors( ) {
+
+  var something = 'something'
+} 
+
+errors();
---
+++ src/warnings.js
@@ -0,0 +1,6 @@
+function warnings() {
+  console.log('warning 1');
+  console.log('warning 2');
+}
+
+warnings();
